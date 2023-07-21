6bad73021bca0a393bffa21c8c5b24ec7413563b
MichalKinas <michal.kinas@hyland.com>
2023-07-21 12:19:15 +0200

    [ACS-4974] Remove * from path as it causes exception

--- lib/content-services/src/lib/document-list/services/custom-resources.service.ts
+++ lib/content-services/src/lib/document-list/services/custom-resources.service.ts
@@ -97,7 +97,8 @@ export class CustomResourcesService {
     getRecentFiles(personId: string, pagination: PaginationModel, filters?: string[]): Observable<NodePaging> {
         const defaultFilter = [
             'TYPE:"content"',
-            '-PATH:"//cm:wiki/*"',
+            //TODO '*' for now cause exception to be thrown, Team6 is working on it
+            '-PATH:"//cm:wiki/"',
             '-TYPE:"app:filelink"',
             '-TYPE:"cm:thumbnail"',
             '-TYPE:"cm:failedThumbnail"',
